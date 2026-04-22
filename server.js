f79fa9d911db824410da619aa559d7a91e7a5163
sorrowpig <soso26674@gmail.com>
2026-04-22 13:27:23 +0800

    Add CORS support to the Express server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // 同時支援 OpenAI & Other
